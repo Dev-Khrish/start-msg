e83e835348d0b338f4b735d387f8e5b7c16274fe
Khrish <106378292+SpidyKhrish007@users.noreply.github.com>
2023-08-30 14:08:25 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,20 +1,20 @@
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext
+from telegram.ext import Updater, MessageHandler, Filters, CallbackContext
 
 # Replace 'YOUR_BOT_TOKEN' with your actual bot token
 BOT_TOKEN = '6286222522:AAGDmZF5xdpakB8_4-SpmATSjerBVG4iohs'
 
-def start(update: Update, context: CallbackContext) -> None:
+def respond_to_message(update: Update, context: CallbackContext) -> None:
     user = update.effective_user
-    update.message.reply_text(f"Hi {user.first_name}!\nThis Bot Is No More Working New BOT LInk Is\n@MHA_SearchBot")
+    update.message.reply_text(f"Hi {user.first_name}!\nThis Bot Is No More Working New BOT LInk Is\n\n@MHA_SearchBot")
 
 def main():
     # Create an Updater object with your bot token
     updater = Updater(token=BOT_TOKEN, use_context=True)
     dispatcher = updater.dispatcher
 
-    # Add a command handler for the /start command
-    dispatcher.add_handler(CommandHandler("start", start))
+    # Add a message handler to respond to messages in private chats
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command & Filters.private, respond_to_message))
 
     # Start polling for updates from Telegram
     updater.start_polling()
