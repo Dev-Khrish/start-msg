f3b9b84e14404974437e7d5cc610f17729361cc3
Khrish <106378292+SpidyKhrish007@users.noreply.github.com>
2023-08-30 14:14:43 +0530

    Update start.py

--- start.py
+++ start.py
@@ -6,6 +6,7 @@ BOT_TOKEN = '6286222522:AAGDmZF5xdpakB8_4-SpmATSjerBVG4iohs'
 
 def respond_to_message(update: Update, context: CallbackContext) -> None:
     user = update.effective_user
+    message = update.message.text
     update.message.reply_text(f"Hi {user.first_name}!\nThis Bot Is No More Working New BOT LInk Is\n\n@MHA_SearchBot")
 
 def main():
@@ -14,7 +15,7 @@ def main():
     dispatcher = updater.dispatcher
 
     # Add a message handler to respond to messages in private chats
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command & Filters.chat_type.private, respond_to_message))
+    dispatcher.add_handler(MessageHandler(Filters.text, respond_to_message))
 
     # Start polling for updates from Telegram
     updater.start_polling()
@@ -24,30 +25,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext
-
-# Replace 'YOUR_BOT_TOKEN' with your actual bot token
-BOT_TOKEN = '6286222522:AAGDmZF5xdpakB8_4-SpmATSjerBVG4iohs'
-
-def start(update: Update, context: CallbackContext) -> None:
-    user = update.effective_user
-    update.message.reply_text(f"Hi {user.first_name}!\nThis Bot Is No More Working New BOT LInk Is\n\n@MHA_SearchBot")
-
-def main():
-    # Create an Updater object with your bot token
-    updater = Updater(token=BOT_TOKEN, use_context=True)
-    dispatcher = updater.dispatcher
-
-    # Add a command handler for the /start command
-    dispatcher.add_handler(CommandHandler("start", start))
-
-    # Start polling for updates from Telegram
-    updater.start_polling()
-
-    # Run the bot until you press Ctrl-C
-    updater.idle()
-
-if __name__ == '__main__':
-    main()
-
