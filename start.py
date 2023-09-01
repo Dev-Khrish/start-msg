a3741db9c74a4b87af4a87ea9abb24a41763f05b
Khrish <106378292+SpidyKhrish007@users.noreply.github.com>
2023-09-01 18:26:54 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,6 +1,6 @@
 import logging
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from telegram.ext import Updater, CommandHandler, MessageHandler, CallbackContext
+from telegram.ext.filters import Filters
 
 # Set up logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
