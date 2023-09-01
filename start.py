9510dcd11df26f3870b3ca7d86d90ccba274601d
Khrish <106378292+SpidyKhrish007@users.noreply.github.com>
2023-09-01 18:35:05 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,9 +1,9 @@
 from pyrogram import Client, filters
 
 # Replace 'YOUR_API_ID' and 'YOUR_API_HASH' with your actual values
-api_id = 'YOUR_API_ID'
-api_hash = 'YOUR_API_HASH'
-bot_token = 'YOUR_BOT_TOKEN'
+api_id = '23830477'
+api_hash = '19f8365d98fb11c9cd6c1eaa8b1fa4b8'
+bot_token = '6286222522:AAGDmZF5xdpakB8_4-SpmATSjerBVG4iohs'
 
 # Create a Pyrogram Client
 app = Client('my_bot', api_id=api_id, api_hash=api_hash, bot_token=bot_token)
