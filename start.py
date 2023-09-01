4c6bc858877b9e2ef3e2c973fe59582c9809692b
Khrish <106378292+SpidyKhrish007@users.noreply.github.com>
2023-09-01 18:23:50 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,26 +1,41 @@
+import logging
 from telegram import Update
-from telegram.ext import Updater, MessageHandler, CallbackContext, Filters
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
-# Replace 'YOUR_BOT_TOKEN' with your actual bot token
-BOT_TOKEN = '6286222522:AAGDmZF5xdpakB8_4-SpmATSjerBVG4iohs'
+# Set up logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
-def respond_to_message(update: Update, context: CallbackContext) -> None:
+# Define your bot token here
+TOKEN = '6286222522:AAGDmZF5xdpakB8_4-SpmATSjerBVG4iohs'
+
+# Function to handle the /start command
+def start(update: Update, context: CallbackContext):
     user = update.effective_user
-    message = update.message.text
-    update.message.reply_text(f"Hi {user.first_name}!\nThis Bot Is No More Working New BOT Link Is\n\n@MHA_SearchBot")
+    update.message.reply_html(
+        f"Hi {user.first_name}!\nThis Bot Is No More Working New BOT Link Is\n\n@MHA_SearchBot"
+    )
+
+# Function to handle incoming messages
+def echo(update: Update, context: CallbackContext):
+    update.message.reply_text(update.message.text)
 
 def main():
-    # Create an Updater object with your bot token
-    updater = Updater(token=BOT_TOKEN, use_context=True)
-    dispatcher = updater.dispatcher
+    # Create the Updater with your bot's token
+    updater = Updater(TOKEN, use_context=True)
 
-    # Add a message handler to respond to messages in private chats
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, respond_to_message))
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
 
-    # Start polling for updates from Telegram
+    # Register a command handler for /start
+    dp.add_handler(CommandHandler("start", start))
+
+    # Register a message handler to echo messages
+    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+
+    # Start the bot
     updater.start_polling()
 
-    # Run the bot until you press Ctrl-C
+    # Run the bot until you send a signal to stop (e.g., Ctrl+C)
     updater.idle()
 
 if __name__ == '__main__':
